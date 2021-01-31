2414406f0466e853b14f56f1ecdd6623362bdc98
Larry Xu <hilarryxu@gmail.com>
2021-01-31 22:27:26 +0800

    build: add build.bat

---
+++ build.bat
@@ -0,0 +1,20 @@
+@echo off
+cd /d %~dp0
+
+REM 编译需要如下工具：
+REM   i586-elf-gcc 交叉编译链
+REM   fasm
+REM   ninja
+REM   python (如果已经生成好 build.ninja，可以不需要)
+
+REM 运行需要：
+REM   bochs
+
+REM set path=%path%;.\scripts
+
+python configure.py
+
+ninja -t clean
+ninja
+
+pause
